902c89ac4f7d3cc55a611496d2bac57ad595dad8
riccardo.murri <riccardo.murri@665f033c-1bef-6b28-aea3-15ebb8bbb050>
2011-02-26 17:38:38 +0000

    Fix Boost Boost compilation in deps installation script.
      modified:
        util/prereq.sh
        util/schroedinger/install-prereq.sh
    
    Original commit message in repo xenia:rheinfall (rev 177)
       revno: 177
       committer: Riccardo Murri <riccardo.murri@gmail.com>
       branch nick: rheinfall
       timestamp: Wed 2010-11-24 22:20:12 +0100

--- util/schroedinger/install-prereq.sh
+++ util/schroedinger/install-prereq.sh
@@ -279,9 +279,7 @@ if [ -n "$BOOST" ]; then
 # told to by the following line:
 using mpi : mpicxx ;
 EOF
-    # first, need to build `bjam`
-    (cd tools/jam/src; sh ./build.sh)
-    # then, build Boost with the new `bjam`
+    # build Boost with the new `bjam`
     PATH=$(pwd)/tools/jam/src/bin.$(uname -s | tr A-Z a-z)$(uname -m):$PATH
     export PATH
     ./bjam --prefix=${sw} toolset=${toolset} threading=multi variant=release install
