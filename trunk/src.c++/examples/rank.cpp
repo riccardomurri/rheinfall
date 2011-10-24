bd744c595d5cf310f863e969a7af2bff7798411e
riccardo.murri <riccardo.murri@665f033c-1bef-6b28-aea3-15ebb8bbb050>
2011-10-24 13:33:42 +0000

    Alert if using the `-p` option in non-threshold pivoting modes.

--- trunk/src.c++/examples/rank.cpp
+++ trunk/src.c++/examples/rank.cpp
@@ -452,6 +452,7 @@ main(int argc, char** argv)
 #else
     else if ('p' == c) {
       // threshold for pivoting
+# if (RF_PIVOT_STRATEGY == RF_PIVOT_THRESHOLD)
       std::istringstream arg(optarg);
       arg >> pivot_threshold;
       if (pivot_threshold <= 0) {
@@ -459,6 +460,11 @@ main(int argc, char** argv)
                   << " Aborting." << std::endl;
         return 1;
       };
+# else
+      std::cerr << "Warning: Ignoring option -p/--pivot-threshold,"
+                << " which only makes sense in threshold pivoting mode."
+                << std::endl;
+# endif
     }
 #endif
     else if ('t' == c) {
