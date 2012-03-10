1a9d5700636f9883b7ed9f4968a8ce565c4d93b3
riccardo.murri <riccardo.murri@665f033c-1bef-6b28-aea3-15ebb8bbb050>
2012-03-10 09:17:19 +0000

    Do not define RF_STATS by default.
    It's easier to define it when needed than undefine it in all other cases.
    
      modified:   rheinfall/config.hpp

--- src.c++/rheinfall/config.hpp
+++ src.c++/rheinfall/config.hpp
@@ -7,7 +7,7 @@
  * @version $Revision$
  */
 /*
- * Copyright (c) 2010, 2011 riccardo.murri@gmail.com. All rights reserved.
+ * Copyright (c) 2010, 2011, 2012 riccardo.murri@gmail.com. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -52,7 +52,7 @@
 
 
 // uncomment/define to enable computing the arithmetic operations count
-#define RF_ENABLE_STATS
+//#define RF_ENABLE_STATS
 
 
 // use WITH_MPI_SERIALIZED / WITH_MPI_MULTIPLE to select which
